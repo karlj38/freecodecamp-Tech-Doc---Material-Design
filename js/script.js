95481c3601f0b0fca6716a0a61bf53213660cbff
Karl Jones <karlj38@gmail.com>
2020-07-26 13:21:43 +0100

    Initial Upload - All tests pass

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function w3_open() {
+  document.getElementById("navbar").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("navbar").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
